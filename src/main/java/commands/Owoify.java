be881ce3620af210491be453532def600ceda3a7
Chehui Chou <deadshot465@gmail.com>
2021-08-02 18:05:12 +0900

    Add Owoify.

---
+++ src/main/java/commands/Owoify.java
@@ -0,0 +1,14 @@
+package commands;
+
+import discord4j.core.DiscordClient;
+import discord4j.core.object.entity.Message;
+import interfaces.Command;
+
+import java.util.Vector;
+
+public class Owoify implements Command {
+    @Override
+    public void run(DiscordClient client, Message msg, Vector<String> args) {
+
+    }
+}
