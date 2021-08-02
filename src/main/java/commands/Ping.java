0a19feb6327f29c3d2ea029251c7615e7f15938b
starrysky-eng <wei5899584@gmail.com>
2021-08-02 17:19:50 +0900

    Update Ping.

--- src/main/java/commands/Ping.java
+++ src/main/java/commands/Ping.java
@@ -9,12 +9,6 @@ import java.util.Vector;
 public class Ping implements Command {
     @Override
     public void run(DiscordClient client, Message msg, Vector<String> args) {
-
-        for (var s :
-                args) {
-            System.out.println(s);
-        }
-
         final var channel = msg.getChannel().block();
         channel.createMessage("Hello").block();
     }
